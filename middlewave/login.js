4a72fa125ceaacc25b3d469acf920bfb9bb3d6fb
nuraalhaji <you@example.com>
2025-08-29 12:56:55 +0100

    commit

---
+++ middlewave/login.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken")
+
+const{ Products }= require("../models/user");
+
+ const userLogin = async (req,res,next) =>{
+  try {
+    const {user, password} = req.body;
+
+    console.log(user,password)
+    const teacher = await Products.findOne({user:user,pass:password})
+    if(!teacher) {
+        res.status(404).json("not found")
+    }
+    const token = jwt.sign({user:teacher.user,pass:teacher.pass},process.env.secret)
+    console.log(token)
+    res.send(res.json(teacher))
+    
+    next()
+  } catch (error) {
+   console.log(error) 
+  }
+}
+
+
+const userVerify = async (req,res,next)=>{
+    const {header,name,password} = req.body
+
+    try {
+       const token = await header;
+       const teacher = await Products.findOne({user:name,pass:password})
+       if(!token){
+        console.log("access denied")
+       } 
+       const verified = jwt.verify(token, process.env.secret)
+       req.name = verified;
+       req.password = verified;
+       res.send(res.json(teacher))
+       next()
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports = {userLogin, userVerify}
--- router/router.js
+++ router/router.js
@@ -2,10 +2,13 @@ const express = require("express")
 const mtnRouter = express.Router('')
 const userRouter = express.Router('')
 const {share,gifting,getOneUser,createUser,getUsers} = require("../controllers/mtn")
+const { userLogin, userVerify } = require("../middlewave/login")
 
 mtnRouter.get("/gifting",gifting)
 mtnRouter.get("/share",share)
 userRouter.post("/",createUser)
+userRouter.post("/login",userLogin)
+userRouter.post("/verify",userVerify)
 userRouter.get("/one",getOneUser)
 userRouter.get("/all",getUsers)
 
